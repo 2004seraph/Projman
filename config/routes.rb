0ade38ceae43c1a272f2abdf3e31e3c655cd9e1a
ack21jb <jbala1@sheffield.ac.uk>
2024-03-11 01:07:39 +0000

    [Feature] Most basic navigation

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
+
+  get '/projects', to: 'projects#index'
+  get '/students', to: 'students#index'
+  get '/issues', to: 'issues#index'
 end
