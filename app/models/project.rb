3629f1d133d8d3bfb98d23e1dfc663824a4d0a1d
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2024-03-20 21:09:04 +0000

    [Fix] Student regex validation, pip dependencies

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,9 @@
-class Project < ApplicationRecord
-  has_many :milestones
+class Student < ApplicationRecord
+  enum :fee_status, { home: 0, overseas: 1 }
+
   has_many :groups
+  has_many :modules
+
+  has_many :assigned_facilitators
 
 end
