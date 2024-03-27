097ddf53fb80462142d201cbf696eda588bb3366
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2024-03-27 14:31:27 +0000

    [Fix] duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "pages#home"
 
-  # get '/projects', to: 'projects#index'
-  # get '/projects/new', to: 'projects#new'
-
-  resources :course_projects, only: [:index, :new] do
+  resources :projects, only: [:index, :new], controller: :course_projects do
     post 'new_project_add_project_choice', on: :collection
   end
-  get '/projects', to: 'course_projects#index', as: 'projects'
-  get '/projects/new', to: 'course_projects#new', as: 'new_project'
+  get '/projects/:id/teams', to: 'lead#teams'
 
   get '/students', to: 'students#index'
   get '/issues', to: 'issues#index'
@@ -22,8 +15,5 @@ Rails.application.routes.draw do
   get '/facilitators/marking/:module', to: 'facilitators#marking'
   get '/facilitators/team/:id', to: 'facilitators#team'
 
-  get '/projects/:id/teams', to: 'lead#teams'
-
   resources :admin
-
 end
