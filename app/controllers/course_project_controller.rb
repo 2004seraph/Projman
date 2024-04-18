50b94b62ebb71001e43afe185174594d650d7e96
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2024-04-18 21:21:32 +0100

    [Refactor] group validations

--- app/controllers/course_project_controller.rb
+++ app/controllers/course_project_controller.rb
@@ -16,15 +16,22 @@ require 'json'
 #   {"Name": "Peer Review", "Date: "dd/mm/yyyy"}]
 
 class CourseProjectController < ApplicationController
-    load_and_authorize_resource
+    load_and_authorize_resource, except: [
+        :add_project_milestone,
+        :remove_project_milestone,
+        :add_project_choice,
+        :remove_project_choice
+    ]
 
-    skip_before_action :verify_authenticity_token, only: [:remove_project_choice,
+    skip_before_action :verify_authenticity_token, only: [
+        :remove_project_choice,
         :clear_facilitator_selection,
         :toggle_project_choices,
         :remove_project_milestone,
         :remove_from_facilitator_selection,
         :remove_facilitator,
-        :create]
+        :create
+    ]
 
     def index
         if current_user.is_staff?
