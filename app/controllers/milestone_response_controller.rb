714772b64fa70566e0579f53eede8a19533499ca
Adam Willis <awillis4@sheffield.ac.uk>
2024-04-19 14:41:50 +0100

    Preference Form replies now create MilestoneResponse objects

--- app/controllers/milestone_response_controller.rb
+++ app/controllers/milestone_response_controller.rb
@@ -2,12 +2,28 @@ class MilestoneResponseController < ApplicationController
     load_and_authorize_resource
 
     def create
-        redirect_to '/projects', notice: "#{params[:author]}. #{params[:project_id]}"
+
+        preferred_teammates = extract_teammates(params, :preferred_teammate)
+        avoided_teammates = extract_teammates(params, :avoided_teammate)
+
+        json_data = {
+            preferred: preferred_teammates,
+            avoided: avoided_teammates
+        }.to_json
+
+        MilestoneResponse.create(json_data: json_data, milestone_id: params[:milestone_id], student_id: current_user.student.id)
     end
 
     private
 
-    def milestone_response_params
+    def extract_teammates(params, key)
+        teammates = []
+        params.each do |param_key, value|
+          if param_key.to_s.start_with?(key.to_s)
+            teammates << value.strip
+          end
+        end
+        teammates
     end
 
 end
