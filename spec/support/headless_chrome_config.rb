273e2a60fc6e96b9abbc177b9ef0aa563ba4be3c
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2024-04-20 17:08:16 +0100

    [Feature] enabled javascript testing

--- spec/support/headless_chrome_config.rb
+++ spec/support/headless_chrome_config.rb
@@ -8,7 +8,7 @@ Capybara.register_driver :headless_chrome do |app|
   chrome_options.add_argument('--disable-extensions')
   chrome_options.add_argument('--disable-popup-blocking')
   chrome_options.add_argument('--window-size=1920,1080')
-
+  chrome_options.add_argument('--enable-javascript')
   chrome_options.add_preference(:download, directory_upgrade: true, prompt_for_download: false, default_directory: '/tmp')
   chrome_options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
 
