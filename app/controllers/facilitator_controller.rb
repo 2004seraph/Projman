5e6622a5ff4a2356eb40f98cd4a20f85ac1dfefb
Oliver Pickford <opickford1@sheffield.ac.uk>
2024-04-21 22:21:44 +0100

    Implement form editing and fix lots of bugs.

--- app/controllers/facilitator_controller.rb
+++ app/controllers/facilitator_controller.rb
@@ -14,30 +14,31 @@ class FacilitatorController < ApplicationController
             @assigned_facilitators = get_assigned_facilitators
 
         else
-            # NOTE: I am assuming here that a facilitator will only be asked to facilitate for teams in projects
-            #       that they're already a facilitator for
+            # I am assuming here that a facilitator will only be asked to facilitate for teams in projects
+            # that they're already a facilitator for.
 
             # TODO: Need to test
             facilitator_project_ids = get_assigned_facilitators.map{|x| x.course_project_id}
             @assigned_facilitators = AssignedFacilitator.where(course_project_id: facilitator_project_ids)
         end
-
+        
         unless params[:projects_filter] == "All" || params[:projects_filter].empty?
             target_project_id = CourseProject.find_by(name: params[:projects_filter]).id
-            @assigned_facilitators = @assigned_facilitators.select{|x| x.course_project_id == target_project_id}
+            @assigned_facilitators = @assigned_facilitators.where(course_project_id: target_project_id)
         end
 
         set_assigned_projects
         render partial: "teams-list-card"
     end
 
-
     def progress_form
         authorize! :read, :facilitator
 
-        set_group
+        set_current_group
                 
-        @progress_form = Milestone.select{|m| m.course_project_id == @current_group.course_project_id && m.json_data["release_date"] == params[:release_date]}.first
+        @progress_form = get_progress_forms_for_group.select{|m|
+            m.json_data["release_date"] == params[:release_date]
+        }.first
         session[:progress_form_id] = @progress_form.id
 
         # Create initial milestone response if we don't have one
@@ -51,20 +52,21 @@ class FacilitatorController < ApplicationController
             # TODO: Set these?
             #  staff_id     :bigint
             #  student_id   :bigint
-            # TODO: Fill in
             @progress_response = MilestoneResponse.new(
                 json_data: {
                     group_id: @current_group.id, 
                     attendance: [],
                     question_responses: [],
-                    facilitator_repr: get_facilitator_repr(get_assigned_facilitators.where(course_project_id: @current_group.course_project_id).first)
+                    facilitator_repr: get_facilitator_repr(
+                        get_assigned_facilitators.where(course_project_id: @current_group.course_project_id).first
+                    )
                 },
                 milestone_id: @progress_form.id
             )
 
             # Handle save failure
             unless @progress_response.save
-                puts "\n! ! ! FAILED TO SAVE\n"
+                puts "[ERROR] TODO: Must handle progress response save failure."
             end
         end
     end
@@ -74,15 +76,23 @@ class FacilitatorController < ApplicationController
     end
 
     def team
+        # Display team/group area
         team = Group.find(params[:id])
         authorize! :read, team
 
-        set_group
+        set_current_group
+        
+        # Get progress forms and sort by release date
+        @progress_forms = get_progress_forms_for_group.sort_by{ 
+            |m| Date.strptime(m.json_data["release_date"], "%Y-%m-%d") 
+        }
 
-        @progress_forms = Milestone.where(course_project_id: @current_group.course_project_id).sort_by{ |m| Date.strptime(m.json_data["release_date"], "%Y-%m-%d") }
-        @progress_forms_submitted = @progress_forms.select{|pf| pf.milestone_responses.select{|mr| mr.json_data["group_id"] == @current_group.id}.length > 0}
+        # Split into sections
+        @progress_forms_submitted = @progress_forms.select{
+            |pf| pf.milestone_responses.select{|mr| mr.json_data["group_id"] == @current_group.id}.length > 0
+        }
+        
         @progress_forms_todo = @progress_forms - @progress_forms_submitted
-
     end
 
     def set_assigned_projects
@@ -90,8 +100,6 @@ class FacilitatorController < ApplicationController
     end
 
     def update_progress_form_response
-        # TODO: SHOULD SAVE LAST UPDATED BY ID...
-
         @current_group = Group.find(session[:team_id])
 
         # Update milestone response for progress form
@@ -102,23 +110,26 @@ class FacilitatorController < ApplicationController
 
         if @progress_response.nil?
             # TODO: Handle error here
-            puts "THIS SHOULD NOT HAPPEN BUT SHOULD PROBABLY HANDLE"
+            puts "[ERROR] Progress response was nil when tried to update, should handle this properly."
         else
             @progress_response.json_data[:attendance] = params[:attendance]
             @progress_response.json_data[:question_responses] = params[:question_responses]
 
-            # TODO: Show name and email, gotta figure out getting staff name.
-            @progress_response.json_data[:facilitator_repr] = get_facilitator_repr(get_assigned_facilitators.where(course_project_id: @current_group.course_project_id).first)
+            # TODO: Show facilitator name and email, gotta figure out getting staff name.
+            @progress_response.json_data[:facilitator_repr] = get_facilitator_repr(
+                get_assigned_facilitators.where(course_project_id: @current_group.course_project_id).first
+            )
+
         end
 
         unless @progress_response.save
-            puts "FAILED TO UPDATE MILESTONE RESPONSE, HANDLE"
+            # TODO: Handle error
+            puts "[ERROR] Progress response failed to save, should handle this properly."
             return render json: { status: "error", message: "Failed to save milestone when save_form." }
         end
 
         # TODO: Scuffed but works, should make better later on.
-        render json: { status: "success", redirect: facilitator_team_path(team_id: session[:team_id]) }
-        
+        render json: { status: "success", redirect: facilitator_team_path(team_id: session[:team_id]) } 
     end
 
     private
@@ -132,25 +143,32 @@ class FacilitatorController < ApplicationController
         end
     end
 
-        def set_group
+        def set_current_group
             # TODO: Need to switch everything to group instead of team to match models
-            # TODO: Stop the symbols and just use session as this is used alot ?
+            # TODO: Stop the symbols and just use session as this is used alot maybe? Gets very confusing
             session[:team_id] = params[:team_id]
             @current_group = Group.find(params[:team_id])
             @current_group_facilitator_repr = get_facilitator_repr(@current_group.assigned_facilitator)
         end
 
         def get_facilitator_repr(facilitator)
-            # NOTE: There isn't a name currently for staff members I beleive,
-            # TODO: Use User.rb model?
+            # TODO: Use User.rb model to get staff name... but can't figure this out?
 
-        # TODO: Test student
-        if facilitator.student_id
-            student = Student.find(facilitator.student_id)
-            return "#{student.forename} #{student.surname}"
-        elsif facilitator.staff_id
-            return Staff.find(facilitator.staff_id).email
+            # TODO: Test student works
+            if facilitator.student_id
+                student = Student.find_by(id: facilitator.student_id)
+                return "#{student.forename} #{student.surname}"
+
+            elsif facilitator.staff_id
+                return Staff.find_by(id: facilitator.staff_id).email
+            end
         end
-        # TODO: Handle better?
-    end
+
+        def get_progress_forms_for_group
+            Milestone.select{
+                |m| m.json_data["name"] == "progress_form" && 
+                Date.parse(m.json_data["release_date"]) <= Date.today && # Only get released forms 
+                m.course_project_id == @current_group.course_project_id
+            }
+        end
 end
