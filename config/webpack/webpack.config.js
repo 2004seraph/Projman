a113090aafcba00ae4d0d68cb410633418bd06bc
Josh Henson <jhenson2@sheffield.ac.uk>
2024-04-24 12:17:31 +0100

    Jquery config fix

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,9 +1,18 @@
 const { generateWebpackConfig, merge } = require('shakapacker')
+const webpack = require('webpack');
 
 const options = {
   resolve: {
     extensions: ['.css', '.scss']
-  }
-}
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+  ]
+};
 
 module.exports = merge({}, generateWebpackConfig(), options)
