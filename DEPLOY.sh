d1b8ea263dadc19d06c30faef2330d9ddcad4e52
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2024-04-30 09:05:45 +0100

    [Fix] epi deploy environment set to demo

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -3,6 +3,6 @@
 # You must be connected to the UoS VPN, and have your repository SSH key configured correctly
 # to run this script
 
-bundle exec epi_deploy release -d development
+bundle exec epi_deploy release -d demo
 bundle exec cap demo deploy:seed
 bundle exec cap demo whenever:update_crontab
