20f867d08f31bce2560c4d10326d929c17338740
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2024-05-17 17:27:54 +0100

    np complete code added

--- spec/models/course_project_spec.rb
+++ spec/models/course_project_spec.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: course_projects
+#
+#  id                        :bigint           not null, primary key
+#  avoided_teammates         :integer          default(0)
+#  markscheme_json           :json
+#  name                      :string           default("Unnamed Project"), not null
+#  preferred_teammates       :integer          default(0)
+#  status                    :enum             default("draft"), not null
+#  team_allocation           :enum
+#  team_size                 :integer          not null
+#  teams_from_project_choice :boolean          default(FALSE), not null
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  course_module_id          :bigint           not null
+#
+# Indexes
+#
+#  index_course_projects_on_course_module_id  (course_module_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (course_module_id => course_modules.id)
+#
 require "rails_helper"
 
 RSpec.describe CourseProject, type: :model do
