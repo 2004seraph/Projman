49811e237c86ecce72d91cde27274249d66f4d8a
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2024-05-17 16:43:20 +0100

    reenabled redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,20 +30,20 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     Sentry.capture_message("Access denied", level: :warn)
     # gracefully redirect the user to the previous page they were on
-    # AuthHelper.log_exception exception, session
+    AuthHelper.log_exception exception, session
 
-    # if user_initiated_page_request?
-    #   redirect_target =
-    #     if session[:redirect_url]
-    #       session[:redirect_url]
-    #     else
-    #       new_user_session_path
-    #     end
-    #   redirect_to redirect_target, alert: AuthHelper::UNAUTHORIZED_MSG
-    # else
-    #   # fail fast if it was not a get request
-    #   throw exception
-    # end
+    if user_initiated_page_request?
+      redirect_target =
+        if session[:redirect_url]
+          session[:redirect_url]
+        else
+          new_user_session_path
+        end
+      redirect_to redirect_target, alert: AuthHelper::UNAUTHORIZED_MSG
+    else
+      # fail fast if it was not a get request
+      throw exception
+    end
   end
 
   def user_initiated_page_request?
